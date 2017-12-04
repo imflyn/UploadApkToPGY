2c18ecc7df235540f9b2e4a888e917d3800866e9
Flyn <flyn.yu@fandine.com>
2017-12-04 18:07:38 +0800

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# UploadApkToPGY
-上传APK文件到蒲公英的脚本
---
+++ consumer_apk_upload.py
@@ -0,0 +1,34 @@
+import os
+import requests
+import json
+
+workspace = "C:\\Users\\Flyn\\.jenkins\\workspace\\Android Consumer"
+
+apk_path = workspace + "\\consumer\\build\outputs\\apk"
+
+
+def upload_file(path):
+    parents = os.listdir(path)
+    for parent in parents:
+        child = os.path.join(path, parent)
+        if os.path.isdir(child):
+            upload_file(child)
+        else:
+            if child.endswith(".apk"):
+                print(child)
+                result = requests.post(
+                    url="https://www.pgyer.com/apiv2/app/upload",
+                    data={"_api_key": "8bc2faf1cea2b0aa0b18465fd3b7ed47",
+                          },
+                    files={"file": open(child, "rb")}
+                )
+                if result.status_code < 300:
+                    response = result.text
+                    print(response)
+                    print("apk 文件上传成功 链接为：https://www.pgyer.com/" + json.loads(response)['data']['buildShortcutUrl'])
+                else:
+                    print("apk 文件上传到蒲公英失败！！")
+
+
+if __name__ == '__main__':
+    upload_file(apk_path)
