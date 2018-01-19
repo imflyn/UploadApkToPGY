82b0bc79b61376694635fea44f955ec74ed40dfb
Flyn <flyn.yu@fandine.com>
2018-01-19 16:46:52 +0800

    添加注释

--- apk_upload.py
+++ apk_upload.py
@@ -6,9 +6,10 @@ import sys
 import configparser
 from mail_sender import MailSender
 
-workspace = "C:\\Users\\Flyn\\.jenkins\\workspace\\Android Employee"
-
-apk_path = workspace + "\\employee\\build\outputs\\apk"
+# Jenkins工作空间目录
+workspace = "C:\\Users\\Flyn\\.jenkins\\workspace\\Android App"
+# Gradle编译完成生成apk所在的目录
+apk_path = workspace + "\\app\\build\outputs\\apk"
 
 mail_sender = MailSender()
 configparser = configparser.ConfigParser()
@@ -20,15 +21,12 @@ mail_sender.password = configparser.get("Info", "password")
 mail_sender.smtp_server = configparser.get("Info", "smtp_server")
 mail_sender.smtp_server_port = configparser.get("Info", "smtp_server_port")
 
+# 时间格式
 date = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time()))
 
-BRANCH = sys.argv[1]
-BUILD_TYPE = sys.argv[2]
-FLAVORS = sys.argv[3]
-
-print("BRANCH:  " + BRANCH)
-print("BUILD_TYPE:  " + BUILD_TYPE)
-print("FLAVORS:  " + FLAVORS)
+BRANCH = sys.argv[1]  # 参数
+BUILD_TYPE = sys.argv[2]  # 创建类型
+FLAVORS = sys.argv[3]  # 渠道
 
 
 def upload_file(path):
@@ -40,6 +38,7 @@ def upload_file(path):
         else:
             if child.endswith(".apk"):
                 print(child)
+                # Http上传apk文件，官方文档：https://www.pgyer.com/doc/view/api#uploadApp
                 result = requests.post(
                     url="https://www.pgyer.com/apiv2/app/upload",
                     data={"_api_key": "8bc2faf1cea2b0aa0b18465fd3b7ed47",
@@ -49,10 +48,13 @@ def upload_file(path):
                 if result.status_code < 300:
                     response = result.text
                     print(response)
-                    url = "https://www.pgyer.com/" + json.loads(response)['data']['buildShortcutUrl']
+                    # 拼接下载apk的链接
+                    url = "https://www.pgyer.com/" + json.loads(response)['data'][
+                        'buildShortcutUrl']
                     print("apk 文件上传成功 链接为：" + url)
+                    # 上传成功后发送邮件，发送内容格式为文本
                     mail_sender.send(
-                        "[" + date + "] Android Employee " + BRANCH + " " + FLAVORS + " " + BUILD_TYPE + " APK 下载文件 ",
+                        "[" + date + "] Android APP " + BRANCH + " " + FLAVORS + " " + BUILD_TYPE + " APK 下载文件 ",
                         "APK编译时间：" + date + "\n" +
                         "APK 下载链接：" + url,
                         lambda: print("发送邮件成功！！"),
